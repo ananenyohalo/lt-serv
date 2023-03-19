475e1da4736e71f9da82432a8ac21e3cd52b893e
ananenyohalo <9281261+ananenyohalo@users.noreply.github.com>
2023-03-19 18:26:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,49 +1,172 @@
-#!/usr/bin/env node -r esm
-
-import 'localenv';
-import optimist from 'optimist';
-
 import log from 'book';
+import Koa from 'koa';
+import tldjs from 'tldjs';
 import Debug from 'debug';
+import http from 'http';
+import { hri } from 'human-readable-ids';
+import Router from 'koa-router';
 
-import CreateServer from '../server.js';
+import ClientManager from './lib/ClientManager.js';
 
-const debug = Debug('localtunnel');
+const debug = Debug('localtunnel:server');
 
-const argv = optimist
-    .usage('Usage: $0 --port [num]')
-    .options('secure', {
-        default: false,
-        describe: 'use this flag to indicate proxy over https'
-    })
-    .options('port', {
-        default: '80',
-        describe: 'listen on this port for outside requests'
-    })
-    .options('address', {
-        default: '0.0.0.0',
-        describe: 'IP address to bind to'
-    })
-    .options('domain', {
-        describe: 'Specify the base domain name. This is optional if hosting localtunnel from a regular example.com domain. This is required if hosting a localtunnel server from a subdomain (i.e. lt.example.dom where clients will be client-app.lt.example.come)',
-    })
-    .options('max-sockets', {
-        default: 10,
-        describe: 'maximum number of tcp sockets each client is allowed to establish at one time (the tunnels)'
-    })
-    .argv;
+export default function (opt) {
+  opt = opt || {};
 
-if (argv.help) {
-    optimist.showHelp();
-    process.exit();
+  const validHosts = opt.domain ? [opt.domain] : undefined;
+  const myTldjs = tldjs.fromUserSettings({ validHosts });
+  const landingPage = opt.landing || 'https://localtunnel.github.io/www/';
+
+  function GetClientIdFromHostname(hostname) {
+    return myTldjs.getSubdomain(hostname);
+  }
+
+  const manager = new ClientManager(opt);
+
+  const schema = opt.secure ? 'https' : 'http';
+
+  const app = new Koa();
+  const router = new Router();
+
+  router.get('/api/status', async (ctx, next) => {
+    const stats = manager.stats;
+    ctx.body = {
+      tunnels: stats.tunnels,
+      mem: process.memoryUsage(),
+    };
+  });
+
+  router.get('/api/tunnels/:id/status', async (ctx, next) => {
+    const clientId = ctx.params.id;
+    const client = manager.getClient(clientId);
+    if (!client) {
+      ctx.throw(404);
+      return;
+    }
+
+    const stats = client.stats();
+    ctx.body = {
+      connected_sockets: stats.connectedSockets,
+    };
+  });
+
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+
+  // root endpoint
+  app.use(async (ctx, next) => {
+    const path = ctx.request.path;
+
+    // skip anything not on the root path
+    if (path !== '/') {
+      await next();
+      return;
+    }
+
+    const isNewClientRequest = ctx.query['new'] !== undefined;
+    if (isNewClientRequest) {
+      const reqId = hri.random();
+      debug('making new client with id %s', reqId);
+      const info = await manager.newClient(reqId);
+
+      const url = schema + '://' + info.id + '.' + ctx.request.host;
+      info.url = url;
+      ctx.body = info;
+      return;
+    }
+
+    // no new client request, send to landing page
+    ctx.redirect(landingPage);
+  });
+
+  // anything after the / path is a request for a specific client name
+  // This is a backwards compat feature
+  app.use(async (ctx, next) => {
+    const parts = ctx.request.path.split('/');
+
+    // any request with several layers of paths is not allowed
+    // rejects /foo/bar
+    // allow /foo
+    if (parts.length !== 2) {
+      await next();
+      return;
+    }
+
+    const reqId = parts[1];
+
+    // limit requested hostnames to 63 characters
+    if (!/^(?:[a-z0-9][a-z0-9\-]{4,63}[a-z0-9]|[a-z0-9]{4,63})$/.test(reqId)) {
+      const msg =
+        'Invalid subdomain. Subdomains must be lowercase and between 4 and 63 alphanumeric characters.';
+      ctx.status = 403;
+      ctx.body = {
+        message: msg,
+      };
+      return;
+    }
+
+    debug('making new client with id %s', reqId);
+    const info = await manager.newClient(reqId);
+
+    const url = schema + '://' + info.id + '.' + ctx.request.host;
+    info.url = url;
+    ctx.body = info;
+    return;
+  });
+
+  const server = http.createServer();
+
+  const appCallback = app.callback();
+
+  server.on('request', (req, res) => {
+    // without a hostname, we won't know who the request is for
+    const hostname = req.headers.host;
+    if (!hostname) {
+      res.statusCode = 400;
+      res.end('Host header is required');
+      return;
+    }
+
+    const clientId = GetClientIdFromHostname(hostname);
+    if (!clientId) {
+      appCallback(req, res);
+      return;
+    }
+
+    const client = manager.getClient(clientId);
+    if (!client) {
+      res.statusCode = 404;
+      res.end('404');
+      return;
+    }
+
+    client.handleRequest(req, res);
+  });
+
+  server.on('upgrade', (req, socket, head) => {
+    const hostname = req.headers.host;
+    if (!hostname) {
+      socket.destroy();
+      return;
+    }
+
+    const clientId = GetClientIdFromHostname(hostname);
+    if (!clientId) {
+      socket.destroy();
+      return;
+    }
+
+    const client = manager.getClient(clientId);
+    if (!client) {
+      socket.destroy();
+      return;
+    }
+
+    client.handleUpgrade(req, socket);
+  });
+
+  return server;
 }
-
-const server = CreateServer({
-    max_tcp_sockets: process.env.LT_MAX_SOCKETS || 100,
-    secure: process.env.LT_SECURE,
-    domain: process.env.LT_DOMAIN,
-});
-
 server.listen(argv.port, argv.address, () => {
     debug('server listening on port: %d', server.address().port);
 });
